4b85000fc2aa82d3364fc799f33acbad60727846
Akash <hafejmohammadakash@gmail.com>
2025-04-07 23:14:01 -0600

    arrow function, scope, ternury topic learn

--- ternury.js
+++ ternury.js
@@ -4,18 +4,18 @@ const person = {
   isStudent: true
 };
 
-// let sentance = "";
-// if(person.isStudent){
-//     sentance = "he is s student";
-// }else{
-//     sentance = "he is a not student";
-// }
-// console.log(sentance)
+let sentance1 = "";
+if(person.isStudent1){
+    sentance1 = "he is s student";
+}else{
+    sentance1 = "he is a not student";
+}
+console.log(sentance1)
 
 
-// let sentance = "";
-// person.isStudent ?  sentance = "he is s student" : sentance = "he is a not student";
-// console.log(sentance)
+let sentance2 = "";
+person.isStudent2 ?  sentance2 = "he is s student" : sentance2 = "he is a not student";
+console.log(sentance2)
 
 
 let sentance = person.isStudent ? "he is s student" :"he is a not student";
